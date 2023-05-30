e53cbeae8f3967b855e6cd5c1ab683bd46eb5461
pWaw <przemko_wawrzyniak@wp.pl>
2023-05-30 20:00:15 +0200

    MVP EnD oF PrOjEcT

--- Assets/Scripts/ContextReader.cs
+++ Assets/Scripts/ContextReader.cs
@@ -1,14 +1,14 @@
-using System.Collections;
+using Dummiesman;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class ContextReader : MonoBehaviour
 {
-    private string folderPath = "Assets/Resources/stations/";
+    private string folderPath = string.Empty;
     private List<string> files = new List<string>();
     private GameObject context;
 
@@ -16,29 +16,44 @@ public class ContextReader : MonoBehaviour
 
     private void Start()
     {
+        folderPath = Application.dataPath + "/stations/";
         string[] filesInFolder = Directory.GetFiles(folderPath, "*.obj");
         var temp = filesInFolder.ToList<string>();
         foreach (string file in temp)
         {
-            files.Add(GetBaseName(file));
+            //files.Add(GetBaseName(file));
+            files.Add(GetNameWithSufix(file));
         }
         menu.options.Clear();
         foreach (string file in files)
         {
-            menu.options.Add(new TMP_Dropdown.OptionData(file));
+            menu.options.Add(new TMP_Dropdown.OptionData(GetBaseName(file)));
         }
-        context = Instantiate(Resources.Load("stations/" + files[0], typeof(GameObject)) as GameObject);
+        OnOptionChange(0);
     }
 
     public void OnOptionChange(int index)
     {
+        string objPath = folderPath + files[index];
         Destroy(context);
-        context = Instantiate(Resources.Load("stations/" + files[index], typeof(GameObject))) as GameObject;
+        if (File.Exists(objPath))
+        {
+            context = new OBJLoader().Load(objPath);
+        }
+        else
+        {
+            Debug.Log(objPath);
+        }
     }
 
     private string GetBaseName(string name)
     {
         string temp = name.Substring(name.LastIndexOf("/") + 1);
         return temp.Substring(0, temp.LastIndexOf("."));
+    }
+
+    private string GetNameWithSufix(string name)
+    {
+        return name.Substring(name.LastIndexOf("/") + 1);
     }
 }
